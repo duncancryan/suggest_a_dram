d4601d2d0fbd0c5b1d1c1d93919f8cfb49251e3c
ben <benfd@outlook.com>
2020-10-16 14:14:42 +0100

    14:13, 16/10/20 - [BUGFIX] fixed duplicate link saves

--- web-scraper/web-scrape-run.py
+++ web-scraper/web-scrape-run.py
@@ -5,12 +5,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Set main web url & request it
-root_url = "https://www.thewhiskyexchange.com/c/40/single-malt-scotch-whisky"
+# Set count & other requirements
 
-root_result = requests.get(root_url)
+count_less_than = True
+prepend_url = "https://www.thewhiskyexchange.com"
+
+
+# Set main web url & request it
+root_url = "https://www.thewhiskyexchange.com/brands/scotchwhisky"
+
+root_result = requests.get(root_url, headers={'User-Agent': 'Mozilla/5.0'})
 
 root_content = root_result.content
+
 
 # Check if recieve appropriate response
 if (root_result.status_code == 200):
@@ -18,12 +25,54 @@ if (root_result.status_code == 200):
     # Set BeautifulSoup object
     soup = BeautifulSoup(root_content, "html.parser")
 
-    # Grab all URLs for whiskies
-    whisky_urls = []
+    # Grab A-to-Z URLS
+    atz_links = soup.findAll("a", class_="producers-link")
 
-    whisky_anchors = soup.findAll("a", class_="product")
+    # Get the four links to the whisky pages
+    whisky_type_links = []
+    
+    for link in atz_links:
+        whisky_type_links.append(link.attrs['href'])
 
-    for anchor in whisky_anchors:
-        whisky_urls.append(anchor.attrs["href"])
+    # Grab brand URLS
 
-    print(whisky_urls)
+    brand_links = []
+    
+    for type_link in whisky_type_links:
+
+        # Request page for each whisky type
+        type_result = requests.get(f"{prepend_url}{type_link}", {'User-Agent': 'Mozilla/5.0'})
+        type_content = type_result.content
+
+        # Create BeautifulSoup object
+        type_soup = BeautifulSoup(type_content, 'html.parser')
+        
+        # Grab each distillery/brand URLs
+        individual_brand_links = type_soup.findAll("a", class_="az-item-link")
+        for link in individual_brand_links:
+            brand_links.append(link.attrs['href'])
+    
+    # print(brand_links)
+
+    # Grab product URLS 
+    product_links = []
+
+    for brand_link in brand_links:
+
+        count = 0
+
+        # Request page for each brand
+        brand_result = requests.get(f"{prepend_url}{brand_link}", {'User-Agent': 'Mozilla/5.0'})
+        brand_content = brand_result.content
+
+        # Create BeautifulSoup object
+        brand_soup = BeautifulSoup(brand_content, 'html.parser')
+        
+        # Grab each individual whisky URLs
+        individual_product_links = brand_soup.findAll("a", class_="product")
+
+        for link in individual_product_links:
+            if count < 2:
+                product_links.append(link.attrs["href"])
+                print(f"count: {count}")
+                count += 1
