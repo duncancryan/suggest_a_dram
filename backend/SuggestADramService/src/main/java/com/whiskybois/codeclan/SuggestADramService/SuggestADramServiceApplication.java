4b50839958b49e2be5d0707b727aa29b850756c6
ben <benfd@outlook.com>
2020-10-17 11:59:54 +0100

    11:59, 17/10/20 - Added backend folder with Java Spring project folder

---
+++ backend/SuggestADramService/src/main/java/com/whiskybois/codeclan/SuggestADramService/SuggestADramServiceApplication.java
@@ -0,0 +1,13 @@
+package com.whiskybois.codeclan.SuggestADramService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuggestADramServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuggestADramServiceApplication.class, args);
+	}
+
+}
---
+++ backend/SuggestADramService/src/test/java/com/whiskybois/codeclan/SuggestADramService/SuggestADramServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whiskybois.codeclan.SuggestADramService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuggestADramServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
