47e88bbf82de6f849fb1bbde5df57d960c18e0f9
ben <benfd@outlook.com>
2020-10-18 11:13:26 +0100

    11:13, 18/10/20 - Build simple navbar

--- frontend/suggest-a-dram/src/App.js
+++ frontend/suggest-a-dram/src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import NavBar from './NavBar';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import { teal, grey } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  spacing: 8,
+  palette: {
+    primary: {
+      main: grey[900]
+    },
+    secondary: {
+      main: teal[700]
+    }
+  },
+});
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <h1>Hello, I am the App!</h1>
-    </div>
+    <main className="main-container">
 
+      <ThemeProvider theme={theme}>
+
+        <NavBar />
+
+      </ThemeProvider>
+
+    </main>
   );
 }
 
--- frontend/suggest-a-dram/src/NavBar.js
+++ frontend/suggest-a-dram/src/NavBar.js
@@ -1,17 +1,41 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import { Toolbar, Typography, Button, Grid, makeStyles, ButtonGroup} from '@material-ui/core';
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import LocalBarRoundedIcon from '@material-ui/icons/LocalBarRounded';
+import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
+import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
 
 export default function NavBar() {
+
     return (
-        <header>
-            {/* logo image with home route */}
-            <ul>
-                <li className="nav-link">
-                    <a>Home</a>
-                </li>
-                <li className="nav-link">
-                    <a>Whiskies</a>
-                </li>
-            </ul>
-        </header>
+        <AppBar>
+
+            <Toolbar>
+
+                <Grid justify="space-between" container>
+
+                    <Grid item>
+
+                        <Typography variant="h4" >Suggest A Dram</Typography>
+
+                    </Grid>
+
+                    <Grid item>
+
+                        <ButtonGroup variant="contained" color="secondary" size="large">
+                            <Button href="/" startIcon={<HomeRoundedIcon />}>Home</Button>
+                            <Button href="/start" startIcon={<SearchRoundedIcon />}>Suggest a Whisky</Button>
+                            <Button href="/all" startIcon={<LocalBarRoundedIcon />}>All Whiskys</Button>
+                            <Button href="/auth" startIcon={<AccountCircleRoundedIcon />}>Login</Button>
+                        </ButtonGroup>
+
+                    </Grid>
+
+                </Grid>
+
+            </Toolbar>
+
+        </AppBar>
     )
 }
