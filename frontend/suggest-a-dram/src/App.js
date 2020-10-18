6b473b41f33847835a878b72a28cbb6bd95303a6
ben <benfd@outlook.com>
2020-10-18 13:19:51 +0100

    13:19, 18/10/20 - Changed header tags to Typography also moved nav to be declared in App

--- frontend/suggest-a-dram/src/App.js
+++ frontend/suggest-a-dram/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './home-page/HomePage';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import { teal, grey } from '@material-ui/core/colors';
+import NavBar from './NavBar';
 
 const theme = createMuiTheme({
   spacing: 8,
@@ -20,6 +21,8 @@ function App() {
     <main className="main-container">
 
       <ThemeProvider theme={theme}>
+
+        <NavBar />
 
         <HomePage />
 
--- frontend/suggest-a-dram/src/home-page/HomePage.js
+++ frontend/suggest-a-dram/src/home-page/HomePage.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import NavBar from '../NavBar';
 import './HomePage.css'
-import Button from '@material-ui/core/Button';
+import {Button, Typography} from '@material-ui/core';
 
 export default function HomePage() {
     return (
-        <div>
-            <header>
-                <NavBar />
-            </header>
-        
-            <main className="home-description">
-                <h2>Find the perfect Whisky</h2>
-                <h3>Discover the most accurate flavour profiles of Scottish Whiskies</h3>
-                <h4>Simply select your flavour preferences and let us suggest a dram for you</h4>
-                <Button variant="contained" color="secondary">Start</Button>
-            </main>
-        </div>
-
+        <main className="home-description">
+            <Typography variant="h2" gutterBottom>Find the perfect Whisky</Typography>
+            <Typography variant="body1" gutterBottom>Discover the most accurate flavour profiles of Scottish Whiskies</Typography>
+            <Typography variant="body1" gutterBottom>Simply select your flavour preferences and let us suggest a dram for you</Typography>
+            <Button variant="contained" color="secondary" size="large" href="/start">Start</Button>
+        </main>
     )
 }
