72a1ea6b69e9b2763d86dc636d679445c8607be5
Callum Wolfe <wolfe.callum@gmail.com>
2020-10-18 13:06:42 +0100

    adds the description for the home page

--- frontend/suggest-a-dram/src/home-page/HomePage.js
+++ frontend/suggest-a-dram/src/home-page/HomePage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import NavBar from '../NavBar';
+import './HomePage.css'
+import Button from '@material-ui/core/Button';
 
 export default function HomePage() {
     return (
@@ -8,9 +10,11 @@ export default function HomePage() {
                 <NavBar />
             </header>
         
-            <main>
-                <h2></h2>
+            <main className="home-description">
+                <h2>Find the perfect Whisky</h2>
                 <h3>Discover the most accurate flavour profiles of Scottish Whiskies</h3>
+                <h4>Simply select your flavour preferences and let us suggest a dram for you</h4>
+                <Button variant="contained" color="secondary">Start</Button>
             </main>
         </div>
 
