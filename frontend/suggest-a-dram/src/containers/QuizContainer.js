fa5c22508c515f0d0da3a40419495d6bc6eb31d7
Duncan Ryan <duncancryan1@gmail.com>
2020-10-18 14:04:55 +0100

    created barebones of QuizContainer

---
+++ frontend/suggest-a-dram/src/containers/QuizContainer.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import NavBar from '../NavBar';
+
+
+export default class QuizContainer extends Component{
+
+    // Constructor
+    constructor(){
+
+        // State
+        this.state = {
+            body: 0,
+            richness: 0,
+            smoke: 0,
+            sweetness: 0,
+            bottle_image_url: '',
+            character: []
+        }
+    }
+
+    // Methods
+
+    onSliderChange(){
+        
+    }
+
+    onCharacterChange(){
+
+    }
+
+    onProgressChange(){
+
+    }
+
+
+    // Render
+
+    render(){
+        return(
+            <main>
+                <NavBar/>
+                <div className="quiz-content">
+                    <div className="questiuon-box">
+                        I will be the Question Container
+                    </div>
+                    <div className="whisky-fill">
+                        And I shall be the Whiksy Bottle!
+                    </div>
+                </div>
+
+            </main>
+        )
+    }
+}
