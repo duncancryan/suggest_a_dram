15ee264453705a32d07c36fd245f1f67af802961
ben <benfd@outlook.com>
2020-10-19 18:38:29 +0100

    18:37, 19/10/20 - [BUGFIX]  Results page route fixed no longer crashes quiz page

--- frontend/suggest-a-dram/src/containers/QuestionWrapper.js
+++ frontend/suggest-a-dram/src/containers/QuestionWrapper.js
@@ -1,79 +1,79 @@
-import React, {Component, Fragment} from 'react';
-import {BrowserRouter as Route, Router, Switch } from 'react-router-dom';
-import CharacterWrapper from './CharacterWrapper';
-import ResultWrapper from './ResultWrapper';
-import StyleWrapper from './StyleWrapper';
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CharacterWrapper from "./CharacterWrapper";
+import ResultWrapper from "./ResultWrapper";
+import StyleWrapper from "./StyleWrapper";
 
 export default class QuestionWrapper extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            styleWhiskies: [],
-            characterWhiskies: [],
-            rankedWhiskies: []
-        }
-        // Binds
-        this.onSubmitStyle = this.onSubmitStyle.bind(this);
-        this.onCharacterSelect = this.onCharacterSelect.bind(this);
-        this.onCharacterSubmit = this.onCharacterSubmit.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      styleWhiskies: [],
+      characterWhiskies: [],
+      rankedWhiskies: [],
+    };
+    // Binds
+    this.onSubmitStyle = this.onSubmitStyle.bind(this);
+    this.onCharacterSelect = this.onCharacterSelect.bind(this);
+    this.onCharacterSubmit = this.onCharacterSubmit.bind(this);
+  }
+
+  onSubmitStyle(data) {
+    this.setState({ styleWhiskies: data });
+    this.props.onComplete();
+  }
+
+  onCharacterSubmit(data) {
+    this.setState({ characterWhiskies: data });
+    this.props.onComplete();
+  }
+
+  onCharacterSelect() {
+    this.props.onComplete();
+  }
+
+  questionSet() {
+    if (this.props.questionSet === 0) {
+      return <StyleWrapper onProgressChange={this.onSubmitStyle} />;
     }
 
-    onSubmitStyle(data){
-        this.setState({styleWhiskies: data});
-        this.props.onComplete();
-    }
-        
-    onCharacterSubmit(data) {
-        this.setState({characterWhiskies: data});
-        this.props.onComplete();
-    }
-
-    onCharacterSelect(){
-        this.props.onComplete();
+    if (this.props.questionSet > 0) {
+      return (
+        <CharacterWrapper
+          stage={this.props.questionSet}
+          onProgressChange={this.onCharacterSelect}
+          characterSubmit={this.onCharacterSelect}
+          currentWhiskies={this.state.styleWhiskies}
+        />
+      );
     }
 
-    questionSet() {
+    // if (this.state.styleWhiskies.length > 0){
+    // } else {
+    //     return <Typography variant="body1">Sorry couldn't find any whiskies matching your request. Please try again</Typography>
+    // }
+  }
 
-        if (this.props.questionSet === 0){
-            return <StyleWrapper onProgressChange={this.onSubmitStyle} />
-        } 
+  render() {
+    return (
+      <Router>
+        <Fragment>
 
-        if (this.props.questionSet > 0){
-            return (
-            <CharacterWrapper stage={this.props.questionSet} 
-                onProgressChange={this.onCharacterSelect} 
-                characterSubmit={this.onCharacterSelect}
-                currentWhiskies={this.state.styleWhiskies}
-             />
-             )
-        } 
-
-        // if (this.state.styleWhiskies.length > 0){  
-        // } else {
-        //     return <Typography variant="body1">Sorry couldn't find any whiskies matching your request. Please try again</Typography>
-        // }
-
-    }
-
-    render(){
-
-        return(
-            <Fragment>
-                {this.questionSet()}
-
-                <Router>
-                    <Fragment>
-
-                        <Switch>
-                        <Route exact path="/result" render={() => {
-                            return <ResultWrapper whiskies={this.state.characterWhiskies} />
-                        }} />
-                        </Switch>
-
-                    </Fragment>
-                </Router>
-
-            </Fragment>
-        )
-    }
+            {this.questionSet()}
+            
+          <Switch>
+            <Route
+              exact
+              path="/result"
+              render={() => {
+                return (
+                  <ResultWrapper whiskies={this.state.characterWhiskies} />
+                );
+              }}
+            />
+          </Switch>
+        </Fragment>
+      </Router>
+    );
+  }
 }
