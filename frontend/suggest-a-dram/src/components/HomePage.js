735499052a7b4f15317c05c9e27a5b867161d2e4
Callum Wolfe <wolfe.callum@gmail.com>
2020-10-21 19:25:36 +0100

    adds world whiskies to home page

--- frontend/suggest-a-dram/src/components/HomePage.js
+++ frontend/suggest-a-dram/src/components/HomePage.js
@@ -8,7 +8,7 @@ export default function HomePage() {
             <div className="home-description">
                 <Typography variant="h1">Suggest A Dram</Typography>
                 <Typography variant="h3" gutterBottom>Find the perfect Whisky</Typography>
-                <Typography variant="body1" gutterBottom>Discover the most accurate flavour profiles of Scottish Whiskies</Typography>
+                <Typography variant="body1" gutterBottom>Discover the most accurate flavour profiles of Scottish and World Whiskies</Typography>
                 <Typography variant="body1" gutterBottom>Simply select your flavour preferences and let us suggest a dram for you</Typography>
                 <Button variant="contained" color="secondary" size="large" href="/quiz">Start</Button>
             </div>
