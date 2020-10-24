263f7fdcb09695a73611e36562487d64d468bd4a
ben <benfd@outlook.com>
2020-10-24 23:04:47 +0100

    22:57, 24/10/20 - Added packages and imports that allegedly fix the iPhone issue (???)

--- frontend/suggest-a-dram/src/index.js
+++ frontend/suggest-a-dram/src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'core-js/fn/number/is-nan'; 
-import 'core-js/es7/'; 
-import 'core-js/es6/';
 import 'raf/polyfill'; 
 
 ReactDOM.render(
